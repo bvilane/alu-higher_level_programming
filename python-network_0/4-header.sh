1ff4c1e2377c85811157d16e5e0e6cc85a4711e6
bvilane <b.vilane@alustudent.com>
2023-08-04 13:34:49 +0000

    Correction of 4. cURL headers

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 # Check if a URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL to send GET request to
-url="$1"
-
-# Set the header variable
-header="X-HolbertonSchool-User-Id: 98"
-
-# Send the GET request using curl and display the response body
-curl -s -H "$header" "$url"
-
+[ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
