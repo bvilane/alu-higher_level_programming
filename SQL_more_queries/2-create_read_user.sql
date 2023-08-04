ec8e2df04b8cdab112da86d01a78d85dfee84c0a
Bavukile <b.vilane@alustudent.com>
2023-08-04 05:40:50 -0700

    Read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,9 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- Check if the database hbtn_0d_2 exists, if not, create it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Check if the user user_0d_2 exists, if not, create it and set the password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege to user_0d_2 on the hbtn_0d_2 database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
