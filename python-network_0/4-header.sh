922dfe83f6465d793819f4b40dc3d3bb8415df07
bvilane <b.vilane@alustudent.com>
2023-08-04 13:30:49 +0000

    cURL only methods

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# cURL headers 
-curl -sH "X-HolbertonSchool-User-Id:98" "$1"
+
+# Check if a URL argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# URL to send GET request to
+url="$1"
+
+# Set the header variable
+header="X-HolbertonSchool-User-Id: 98"
+
+# Send the GET request using curl and display the response body
+curl -s -H "$header" "$url"
+
