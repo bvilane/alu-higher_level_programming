0295553e7e9fafedfb344c9f535add09d5877b9e
Bavukile <b.vilane@alustudent.com>
2023-10-01 14:40:45 -0700

    Sorted occurences

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -2,12 +2,14 @@
 // Script imports a dictionary of occurrences by user id
 // and computes a dictionary of user ids by occurrence.
 
-const { dict } = require('./101-data.js');
-const Dictn = {};
-for (const N in dict) {
-    if (Dictn[dict[N]] === undefined) {
-	Dictn[dict[N]] = [];
-    }
-    Dictn[dict[N]].push(N);
+const dict = require('./101-data').dict;
+
+let newDic = {};
+for (let key in dict) {
+  if (newDic[dict[key]] === undefined) {
+    newDic[dict[key]] = [];
+  }
+  newDic[dict[key]].push(key);
 }
-console.log(Dictn);
+
+console.log(newDic);
