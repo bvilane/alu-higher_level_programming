c24ddd1b9fd32530099e97990bfcd82b2e978be8
Bavukile <b.vilane@alustudent.com>
2023-10-01 13:07:21 -0700

    updated

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const myVar0 = 'C is fun';
-const myVar1 = 'Python is cool';
-const myVar2 = 'JavaSript is amazing';
-console.log(myVar0);
-console.log(myVar1);
-console.log(myVar2);
+
+console.log('C is fun\nPython is cool\nJavascript is amazing');
